5727692f5c93c0b729df1cb80f357bb1c292c8b2
hussam tarteer <hussamtarteer74@gmail.com>
2025-06-01 12:15:14 +0300

    d

--- src/main/java/HciProject/com/HelpingRequests/dto/RequestDTOtest1.java
+++ src/main/java/HciProject/com/HelpingRequests/dto/RequestDTOtest1.java
@@ -1,5 +1,15 @@
-package HciProject.com.HelpingRequests.dto;
+// package HciProject.com.HelpingRequests.dto;
 
-public record RequestDTO() {
+// import java.time.LocalDate;
+// import java.time.LocalTime;
 
-}
+// public record RequestDTOtest1(String creatorName,String requestBody,String location,LocalDate requestDate,LocalTime requestTime) {
+    
+// }
+// //     private String creatorName;
+// //     private String requestBody;
+// //     private String location;
+// //     private LocalDate requestedDate;
+// //     private LocalTime requestedTime;
+// //     private Long creatorId;
+// //     public RequestDTO() {}
