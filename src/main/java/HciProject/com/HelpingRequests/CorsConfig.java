bc5b655339245fe968d977af77999cb69f6a6a23
hussam tarteer <hussamtarteer74@gmail.com>
2025-06-05 13:35:50 +0300

    fixing cors config

--- src/main/java/HciProject/com/HelpingRequests/CorsConfig.java
+++ src/main/java/HciProject/com/HelpingRequests/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig implements WebMvcConfigurer {
         // Allowing requests from your frontend
         registry.addMapping("/**") // Apply to all endpoints
                 .allowedOrigins("https://y-zer0.github.io")
-                .allowedOrigins("http://localhost:3000")
+                // .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH") // Allow these methods
                 .allowedHeaders("*")
                 .allowCredentials(true); // Allow all headers (you can restrict if needed)
